5ed6093eff29d5de554aa6c837d9553a72f39795
Tim LaTorre <timlatorre@gmail.com>
2012-11-29 11:25:56 -0500

    init

---
+++ quizzes/helium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helium::Application.config.secret_token = '43ea91b0943f3635af169400dc0eb5321cfb422950008b19fd2fdf735efd064065ff97a5ce3ada232ff97693561dc4426be2cfb41131fc10390388fbd84302c4'
---
+++ quizzes/helium/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+('Ninja_1'..'Ninja_500').each do |ninja_name|
+	n = Ninja.create(:name => ninja_name)
+end
